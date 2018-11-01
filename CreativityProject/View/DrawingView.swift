cda2f4cfa82606339cf1512b3b84f82b6c8e4d4c
Caroline Jarman <carolinerosejarman@gmail.com>
2018-11-01 09:46:21 -0600

    Created functions to draw a stick figure, turtle, and smiley face

--- CreativityProject/View/DrawingView.swift
+++ CreativityProject/View/DrawingView.swift
@@ -16,7 +16,9 @@ public class DrawingView: UIView
     // An empty implementation adversely affects performance during animation.
     public override func draw(_ rect: CGRect) -> Void
      {
-    
+        createStickFigure().stroke()
+        drawTurtle()
+        drawSomething().stroke()
         // Drawing code
      }
  
@@ -25,7 +27,65 @@ public class DrawingView: UIView
         let figure : UIBezierPath = UIBezierPath()
         //when a return type of (uibez...) must make a variable of that inside {} and return it
         
+        UIColor.magenta.setStroke()
+        figure.lineWidth = 3.0
+        
+        figure.addArc(withCenter: CGPoint (x: 200, y: 200),
+                      radius: CGFloat (20),
+                      startAngle: CGFloat(0),
+                      endAngle: CGFloat(2) * CGFloat.pi,
+                      clockwise: true)
+        
+        figure.move(to: CGPoint(x: 200, y: 220))
+        figure.addLine(to: CGPoint(x: 200, y: 270))
+        figure.move(to: CGPoint(x: 180, y: 240))
+        figure.addLine (to: CGPoint (x: 220, y: 240))
+        figure.move (to: CGPoint (x: 200, y: 270))
+        figure.addLine(to: CGPoint(x: 180, y: 300))
+        figure.move (to: CGPoint (x: 200, y: 270))
+        figure.addLine(to: CGPoint(x: 220, y: 300))
+
+
+
+        
         return figure
+    }
+    
+    private func drawTurtle() -> Void
+    {
+        let logo = UIBezierPath()
+        UIColor.white.setFill()
+        logo.move(to: CGPoint (x: 50, y: 250))
+        logo.addLine(to: CGPoint (x: 100, y: 300))
+        logo.addLine(to: CGPoint (x: 50, y: 350))
+        logo.close()
+        logo.fill()
+    }
+    
+    private func drawSomething() -> UIBezierPath
+    {
+        let figure1 : UIBezierPath = UIBezierPath()
+        UIColor.black.setStroke()
+        figure1.lineWidth = 3.0
+        
+        figure1.addArc(withCenter: CGPoint (x: 100, y: 100),
+                      radius: CGFloat (20),
+                      startAngle: CGFloat(0),
+                      endAngle: CGFloat(2) * CGFloat.pi,
+                      clockwise: true)
+        
+        figure1.move (to: CGPoint(x: 80, y: 90))
+        figure1.addLine (to: CGPoint(x: 120, y: 90))
+        figure1.move (to: CGPoint(x: 90, y: 90))
+        figure1.addLine (to: CGPoint(x: 90, y: 100))
+        figure1.addLine (to: CGPoint(x: 95, y: 100))
+        figure1.addLine (to: CGPoint(x: 95, y: 90))
+
+
+
+        return figure1
+        
+
     }
     
     
