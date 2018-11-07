a102147c29841a82b5c01f10f296a283085ecae9
Caroline Jarman <carolinerosejarman@gmail.com>
2018-11-07 09:39:34 -0700

    Added art cell

--- CreativityProject/Controller/ArtCollectionViewController.swift
+++ CreativityProject/Controller/ArtCollectionViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Jarman, Caroline on 11/7/18.
 //  Copyright © 2018 ctec. All rights reserved.
 // FIRST MAKE CLASS AND ALL METHODS PUBLIC
-//Queue - lines that can be added to DQ -> deleting the line (from the front)
+//Queue - lines that can be added to DQ (dequeue) -> deleting the line (from the front)
 //Override -> UICollectionView has a method with the same header, override says to use mine not the default
 //super.__________ -> go up to the parent and do THEIR instructions then do whatever else I said
 
@@ -16,40 +16,44 @@ private let reuseIdentifier = "artIdentifier"
 public class ArtCollectionViewController: UICollectionViewController
 {
     
-    //Mark: Data members for Creativity Screen
+    //MARK: Data members for Creativity Screen
     
-    private let sectionInsets = UIEdgeInsets(top : 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    private let sectionInsets = UIEdgeInsets(top : 50.0, left: 20.0, bottom: 50.0, right: 20.0) //gap between items (larger on top/bottom)
     private let itemsPerRowCompact : CGFloat = 4
     private let itemsPerRowNormal : CGFloat = 6
     
     private let creativeCS : [UIImage?] =
     {
         return [
+            UIImage(named: "Octocat"),
             UIImage(named: "AppArt11:7"),
             UIImage(named: "JavaHaiku"),
             UIImage(named: "MainframeHaiku"),
+            UIImage(named: "SwiftHaiku"),
             UIImage(named: "MMSharkImage"),
-            UIImage(named: "Octocat"),
             UIImage(named: "SAImage2"),
             UIImage(named: "SantaJaws"),
-            UIImage(named: "SwiftHaiku")
+
         ]
     }()
     
     private let labels : [String] =
     {
         return [
+        "My Octocat",
         "App Drawn Art",
         "Haiku to Java",
         "Haiku to Mainframe",
+        "Haiku to Swift",
         "Megashark v Mechashark",
-        "My Octocat",
         "5 Headed Shark Attack",
         "Santa Jaws",
-        "Haiku to Swift"
+
         ]
     }()
 
+    //MARK: - Lifecycle
+    
     public override func viewDidLoad() -> Void //life cycle method, always needs view did load-like START for the page
     {
         super.viewDidLoad()
@@ -86,7 +90,7 @@ public class ArtCollectionViewController: UICollectionViewController
         return 0
     }
 
-
+                //_ means no outside name,          (inside name is collection view)
     public override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
     {
         // #warning Incomplete implementation, return the number of items
@@ -103,6 +107,20 @@ public class ArtCollectionViewController: UICollectionViewController
     }
 
     // MARK: UICollectionViewDelegate
+    
+    public func collectionView(_collectionView: UICollectionView,
+                               layout collectionViewLayout: UICollectionViewLayout,
+                               sizeForItemAt indexPath: IndexPath) -> CGSize
+    {
+        let paddingSpace = sectionInsets.left * (itemsPerRowCompact + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRowCompact
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    
+    
 
     /*
     // Uncomment this method to specify if the specified item should be highlighted during tracking
---
+++ CreativityProject/View/ArtCell.swift
@@ -0,0 +1,14 @@
+//
+//  ArtCell.swift
+//  CreativityProject
+//
+//  Created by Jarman, Caroline on 11/7/18.
+//  Copyright © 2018 ctec. All rights reserved.
+//
+
+import UIKit
+
+public class ArtCell: UICollectionViewCell
+{
+    
+}
